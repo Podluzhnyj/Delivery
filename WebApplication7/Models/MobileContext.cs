009f0b713637c4142535a21e08a4beec0c459250
Vladislav <smash8316@gmail.com>
2021-02-18 20:45:20 +0300

    Create class: User, Order. Add Entity Framework.

---
+++ WebApplication7/Models/MobileContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication7.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ WebApplication7/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Login { get; set; } // имя фамилия покупателя
+        public string Address { get; set; } // адрес покупателя
+        public string ContactPhone { get; set; } // контактный телефон покупателя
+
+        public User User { get; set; }
+    }
+}
